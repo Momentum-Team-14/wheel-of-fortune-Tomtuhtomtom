d7390c1d2a6d73393724f7e3431d25ac714dbc5e
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-15 14:01:03 -0400

    updated user ?'s with invalid entries and prepped a blank answer display

--- mystery_word.py
+++ mystery_word.py
@@ -7,10 +7,11 @@ def play_game():
     print('Welcome to Mystery Word!!')
     read_the_rules()
     ask_to_play()
-    with open('test-word.txt', 'r') as words_file:
+    with open('words.txt', 'r') as words_file:
         solution_word = select_random_word(words_file)
     print(solution_word)
-    number_of_letters(solution_word)
+    len_of_answer = number_of_letters(solution_word)
+    show_partial_answer(len_of_answer)
 
 
 # clears the console
@@ -21,23 +22,31 @@ def cls():
 # ask the user to play
 def ask_to_play():
     play = input('Would you like to play? [Y/N] ').upper()
-    if play == 'Y':
-        return
+    if play != 'Y' and play != 'N':
+        print("Invalid Entry")
+        ask_to_play()
     else:
-        exit()
+        if play == 'Y':
+            return
+        else:
+            exit()
 
 
 # ask the user if they want to read the rules
 # need to write rules file
 def read_the_rules():
     rules = input('Would you like to read the rules? [Y/N] ').upper()
-    if rules == 'Y':
-        with open('rules.txt', 'r') as rules_file:
-            rules_txt = rules_file.read()
-            print(rules_txt)
-            return
+    if rules != 'Y' and rules != 'N':
+        print("Invalid Entry")
+        read_the_rules()
     else:
-        return
+        if rules == 'Y':
+            with open('rules.txt', 'r') as rules_file:
+                rules_txt = rules_file.read()
+                print(rules_txt)
+                return
+        else:
+            return
 
 
 # uses txt file to select a random word
@@ -48,13 +57,13 @@ def select_random_word(file):
 
 # tells user how many letters in the word
 def number_of_letters(answer):
-    pass
     print(f'The Mystery Word has {len(answer)} letters!')
+    return len(answer)
 
 
 # displays partial answer
-def show_partial_answer():
-    pass
+def show_partial_answer(answer):
+    print('\n"{:>8}"'.format(answer * "__ "))
 
 
 if __name__ == "__main__":
