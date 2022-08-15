d1698f89bbad734e082060caa77997ed960b7d2f
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-15 17:24:26 -0400

    working on if input is in available letters

--- mystery_word.py
+++ mystery_word.py
@@ -10,11 +10,11 @@ def play_game():
     with open('words.txt', 'r') as words_file:
         solution_word = select_random_word(words_file)
     len_of_answer = number_of_letters(solution_word)
-    show_partial_answer(len_of_answer)
+    blank_answer = show_blank_answer(len_of_answer)
     all_choices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
                'W', 'X', 'Y', 'Z']
-    check_letter(all_choices)
+    check_letter(all_choices, blank_answer)
 
 
 # clears the console
@@ -64,16 +64,22 @@ def number_of_letters(answer):
 
 
 # displays partial answer
-def show_partial_answer(answer):
-    blanks = answer * " __ "
-    print(f'\n " {blanks} "\n')
+def show_blank_answer(answer):
+    blanks = answer * [" __ "]
+    print(f'\n"{" ".join(blanks)}"\n')
+    return blanks
 
 
 # check for guessed letter
-def check_letter(choices_left):
-    print(f'Choices available:\n{" ".join(choices_left)}\n')
+def check_letter(all_choices, blank):
+    print(f'Choices available:\n{" ".join(all_choices)}')
+    print(all_choices)
     user_letter = input('Pick a letter: ').upper()
     print(user_letter)
+    if user_letter in all_choices:
+        choices_left = all_choices.remove(user_letter)
+    print(choices_left)
+    # check_letter(choices_left, blank)
 
 
 if __name__ == "__main__":
