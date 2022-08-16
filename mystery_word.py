4022f63525943c19c19b516938b1663f34993a3b
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-16 19:24:24 -0400

    added color

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,6 @@
 import random
 import os
+from colorama import Fore, Back, Style
 
 ALL_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
@@ -10,7 +11,9 @@ initial_list_for_letters = []
 
 def play_game(choice_letters):
     cls()
-    print(f'{"***Welcome to Mystery Word!!***"}\n')
+    print(Fore.YELLOW)
+    print(f'{"***Welcome to Mystery Word!!***"}')
+    print(Fore.GREEN)
     read_the_rules()
     ask_to_play()
     choice_letters = ALL_LETTERS[:]
@@ -33,15 +36,17 @@ def cls():
 
 # ask the user if they want to read the rules
 def read_the_rules():
-    rules = input('Would you like to read the rules? [Y/N] ').upper()
+    rules = input(Fore.GREEN + 'Would you like to read the rules? [Y/N] ').upper()
     if rules != 'Y' and rules != 'N':
-        print("Invalid Entry")
+        print(Fore.RED + "Invalid Entry")
         read_the_rules()
     else:
         if rules == 'Y':
+            print(Style.RESET_ALL)
             with open('rules.txt', 'r') as rules_file:
                 rules_txt = rules_file.read()
                 print(rules_txt)
+                print(Fore.GREEN)
                 return
         else:
             return
@@ -49,12 +54,13 @@ def read_the_rules():
 
 # ask the user to play
 def ask_to_play():
-    play = input('Are you ready to play? [Y/N] ').upper()
+    play = input(Fore.GREEN + 'Are you ready to play? [Y/N] ').upper()
     if play != 'Y' and play != 'N':
-        print("Invalid Entry")
+        print(Fore.RED + "Invalid Entry")
         ask_to_play()
     else:
         if play == 'N':
+            print(Fore.RED)
             print("Come back whenever you're ready")
             exit()
         else:
@@ -86,6 +92,7 @@ def choose_your_difficulty(file):
 
 
 # uses txt file to select a random word
+# used if changed to no difficulty mode
 def select_random_word(file):
     list_of_words = file.read().replace('\n', ' ').upper().split()
     return random.choice(list_of_words)
@@ -93,6 +100,7 @@ def select_random_word(file):
 
 # tells user how many letters in the word
 def number_of_letters(answer):
+    print(Fore.YELLOW)
     print(f'The Mystery Word has {len(answer)} letters!')
     return len(answer)
 
@@ -100,7 +108,7 @@ def number_of_letters(answer):
 # displays partial answer
 def show_blank_answer(answer):
     blanks = answer * [" __ "]
-    print(f'\n"{" ".join(blanks)}"\n')
+    print(Fore.WHITE + f'\n"{" ".join(blanks)}"\n')
     return blanks
 
 
@@ -111,15 +119,21 @@ def check_letter_availability(choices_left, guess):
             if choices_left[i] == guess:
                 choices_left[i] = ' '
     elif len(guess) > 1:
+        print(Fore.RED)
         print("Too many letters, you have to pick only one letter")
+        print(Fore.GREEN)
         guess = input('Pick a letter: ').upper()
         check_letter_availability(choices_left, guess)
     elif len(guess) == 0:
+        print(Fore.RED)
         print("No Entry, you have to pick a letter")
+        print(Fore.GREEN)
         guess = input('Pick a letter: ').upper()
         check_letter_availability(choices_left, guess)
     else:
+        print(Fore.RED)
         print(f"You've picked {guess} already, try again")
+        print(Fore.GREEN)
         guess = input('Pick a letter: ').upper()
         check_letter_availability(choices_left, guess)
     return guess
@@ -127,21 +141,21 @@ def check_letter_availability(choices_left, guess):
 
 # check if guessed letter is right or wrong
 def guess_the_letter(partial_answer, answer, guesses, letters):
-    print(f'Letters available:\n{" ".join(letters)}\n')
-    user_guess = input('Pick a letter: ').upper()
+    print(Fore.CYAN + f'Letters available:\n{" ".join(letters)}\n')
+    user_guess = input(Fore.GREEN + 'Pick a letter: ').upper()
     user_guess = check_letter_availability(letters, user_guess)
     if user_guess in answer:
         for i in range(len(answer)):
             if user_guess == answer[i]:
                 partial_answer[i] = answer[i]
         cls()
-        print(f'{"Nice guess!"}\n')
-        print(f'"{" ".join(partial_answer)}"\n')
+        print(Fore.YELLOW + f'{"Nice guess!"}\n')
+        print(Fore.WHITE + f'"{" ".join(partial_answer)}"\n')
         if " __ " not in partial_answer:
             cls()
-            print(f'{"You guessed all the letters!!!"}\n')
-            print(f'The Mystery Word was:\n\n{"".join(answer)}\n')
-            print(f'{"YOU WON!    " * 50}\n')
+            print(Fore.YELLOW + f'{"You guessed all the letters!!!"}\n')
+            print(Fore.CYAN + f'The Mystery Word was:\n\n{"".join(answer)}\n')
+            print(Fore.MAGENTA + f'{"YOU WON!    " * 50}\n')
             for i in range(len(ALL_LETTERS)):
                 if letters[i] == ' ':
                     letters[i] = ALL_LETTERS[i]
@@ -151,22 +165,22 @@ def guess_the_letter(partial_answer, answer, guesses, letters):
     else:
         cls()
         guesses -= 1
-        print(f'Sorry, {user_guess} is not in the Mystery Word. You have {guesses} guesses left!\n')
-        print(f'"{" ".join(partial_answer)}"\n')
+        print(Fore.RED + f'Sorry, {user_guess} is not in the Mystery Word. You have {guesses} guesses left!\n')
+        print(Fore.WHITE + f'"{" ".join(partial_answer)}"\n')
         if guesses > 0:
             guess_the_letter(partial_answer, answer, guesses, letters)
         else:
             cls()
-            print(f'{"You are out of guesses"}\n')
-            print(f'The Mystery Word was: \n\n{"".join(answer)}\n\n')
-            print(f'{"GAME OVER"}\n')
+            print(Fore.RED + f'{"You are out of guesses"}\n')
+            print(Fore.WHITE + f'The Mystery Word was: \n\n{"".join(answer)}\n\n')
+            print(Fore.RED + f'{"GAME OVER"}\n')
             try_again(letters)
 
 
 def try_again(letters):
-    again = input('Would you like to play again? [Y/N] ').upper()
+    again = input(Fore.GREEN + 'Would you like to play again? [Y/N] ').upper()
     if again != 'Y' and again != 'N':
-        print("Invalid Entry")
+        print(Fore.RED + "Invalid Entry")
         try_again(letters)
     else:
         if again == 'Y':
